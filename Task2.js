565d1f7336e7f34cc828d8dc5f155ac1e0b83f4b
Motaher Mahmud Rakib <motahermahmudrakib07@gmail.com>
2025-07-28 10:47:13 +0600

    add toFixed in solution

--- Task2.js
+++ Task2.js
@@ -1,10 +1,10 @@
 const math = 75.25;
 const bio = 65;
 const che = 80;
-const phy =34.45;
+const phy =35.45;
 const ban = 99.50;
 
 const totalMarks = math + bio + che +phy + ban;
 const avg = totalMarks/5;
 
-console.log(avg) 
+console.log(avg.toFixed(2)) 
